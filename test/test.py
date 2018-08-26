1889cb01ea35b40f43540485db6e25d2dc7bcb4f
jlha65 <jose-luis1997@hotmail.com>
2018-08-25 20:42:57 -0500

    Added gif creation functionality. Now an input image can be "shaked" with a "triggered" effect.

--- test/test.py
+++ test/test.py
@@ -1,21 +1,80 @@
 import PIL
 from PIL import Image, ImageDraw
+import random
+import time
+import math
 
-names = ['img{:02d}.png'.format(i) for i in range(20)]
-im = Image.new("RGB", (200,200), 'green')
+inputImage = "reinhardt.png"
+auxImage1 = "triggered.jpg"
 
-pos = 0
+duration = 40
+#Generate frames in a list
+names = ['img{:02d}.png'.format(i) for i in range(4)]
+
+#Open Images
+img = Image.open(inputImage)
+triggImg = Image.open("triggered.jpg")
+
+#Convert image to jpg
+rgbimg = img.convert('RGB')
+img = rgbimg
+
+#Adding a red effect to an image
+redimg = Image.open("red.jpg")
+p, q = img.size
+redimg = redimg.resize((p, q), Image.ANTIALIAS)
+h, k = redimg.size
+print(p, q, h, k, img.mode, redimg.mode)
+blendedimg = Image.blend(img, redimg, 0.4)
+img = blendedimg
+
+#Adding the "triggered" to an image
+newimg = img
+p, q = newimg.size
+newimg = newimg.crop((0,0,p,q+math.ceil(q/6)))
+p, q = newimg.size
+newTrigg = triggImg.resize((p, math.ceil(q/6)), Image.ANTIALIAS)
+newimg.paste(newTrigg, (0, math.ceil(q-(q/6))))
+newimg.save('test.png')
+img = newimg
+
+#Counter for the loop
+i = 1
+
+#Generate shaking gif individual images
 for n in names:
-    frame = im.copy()
-    draw = ImageDraw.Draw(frame)
-    draw.ellipse((pos, pos, 50+pos, 50+pos), 'red')
+    frame = img.copy()
+    if i % 4 == 1 :
+        c = 5
+        f = 0
+    elif i % 4 == 2 :
+        c = -5
+        f = 0
+    elif i % 4 == 3 :
+        c = 0
+        f = 5
+    else :
+        c = 0
+        f = -5
+
+    a = 1
+    b = 0
+    d = 0
+    e = 1
+    img = img.transform(img.size, Image.AFFINE, (a, b, c, d, e, f))
+
+    #img.save('image.png')
     frame.save(n)
-    pos += 10
+    i = i+1
 
+#Add generated images to a list
 images = []
-
 for n in names:
     frame = Image.open(n)
-    images.append(frame)
+    #Crop images before appending
+    w, h = frame.size
+    cropped_img = frame.crop((5, 5, w-5, h-5))
+    images.append(cropped_img)
 
-images[0].save('circle.gif', save_all = True, append_images = images[1:], duration = 100, loop = 0)
+#Generate the gif
+images[0].save('circle.gif', save_all = True, append_images = images[1:], duration = duration, loop = 0)
